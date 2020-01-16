db15ad73262c762abed1508d80dd7f83d49862c8
Steven G. Harms <sgharms@stevengharms.com>
2020-01-16 13:35:02 -0500

    src/index.js: Graphviz-friendly Pretty Printing BST

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('hi')
+import BST from 'binary_search_tree'
+import { shuffledRange } from 'shuffle'
+
+const BST_SOURCE = shuffledRange(1, 100)
+const randomValue = Math.floor(Math.random() * BST_SOURCE.length + 1)
+const tree = new BST(BST_SOURCE)
+tree.printBFS()
